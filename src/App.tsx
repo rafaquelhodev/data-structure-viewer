ad5b2699c12e74480e1030c0333d37faee296eaf
Rafael <rafael.quelho@gmail.com>
2021-07-27 21:46:27 -0300

    Add menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Routes from "./main/routes"
+import Menu from "./template/menu";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<div className="container">
+    <Menu></Menu>
+    <Routes></Routes>
+  </div>)
 }
 
 export default App;
