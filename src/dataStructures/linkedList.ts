4d377687e68b30b1e25f21dd7070b6d8bdefeb64
Rafael <rafael.quelho@gmail.com>
2021-07-31 12:14:44 -0300

    Creating wrapper methods to log

--- src/dataStructures/linkedList.ts
+++ src/dataStructures/linkedList.ts
@@ -29,7 +29,30 @@ export class LinkedListDs implements DataStructure<number>{
         this.logger = new Logger();
     }
 
+    addInPosition(value: number, pos: number = this.nElements) {
+        try {
+            this.logger.newLoggerBlock("Adding node");
+            this._addInPosition(value, pos);
+            this.logger.endLogBlock();
+        } catch (ex) {
+            alert(ex);
+        }
+    }
+
+    remove(pos: number = this.nElements - 1) {
+        try {
+            this.logger.newLoggerBlock("Removing node");
+            this._remove(pos);
+            this.logger.endLogBlock();
+        } catch (ex) {
+            alert(ex);
+        }
+    }
+
     add(value: number): void {
+        this.logger.newLoggerBlock("Adding tail node");
+        this.logger.addLogBlock(`Pointing to node to tail`);
+
         const newNode = new Node(value, null);
 
         if (this.nElements === 0) {
@@ -45,12 +68,11 @@ export class LinkedListDs implements DataStructure<number>{
         }
 
         this.nElements += 1;
+
+        this.logger.endLogBlock();
     }
 
-    addInPosition(value: number, pos: number = this.nElements) {
-
-        this.logger.newLoggerBlock("Adding node");
-
+    private _addInPosition(value: number, pos: number = this.nElements) {
         if (pos > this.nElements) {
             throw new Error("The number of elements is less than the required position");
         } else if (pos < 0) {
@@ -90,11 +112,9 @@ export class LinkedListDs implements DataStructure<number>{
         }
 
         prevNode.next = newNode;
-
-        this.logger.endLogBlock();
     }
 
-    remove(pos: number = this.nElements - 1): void {
+    private _remove(pos: number = this.nElements - 1): void {
         if (this.nElements == 0) {
             throw new Error("Error during deletion: the linked list is empty.");
         } else if (pos < 0 || pos >= this.nElements) {
@@ -105,6 +125,8 @@ export class LinkedListDs implements DataStructure<number>{
 
         let temp = this.head;
         if (pos == 0) {
+            this.logger.addLogBlock(`Pointing to node at i = ${pos}`);
+
             this.head = temp == null ? null : temp.next;
             if (this.nElements <= 1) {
                 this.tail = this.head;
@@ -121,6 +143,8 @@ export class LinkedListDs implements DataStructure<number>{
             temp = temp.next;
 
             index += 1;
+
+            this.logger.addLogBlock(`Pointing to node at i = ${index}`);
         }
 
         if (previous == null) {
@@ -132,7 +156,6 @@ export class LinkedListDs implements DataStructure<number>{
         if (pos == this.nElements) {
             this.tail = previous;
         }
-
     }
 
     getHead() {
